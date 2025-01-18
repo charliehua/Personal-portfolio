cb4fd5ccc37601d17f7c6d89b0c685313f1e87f7
ch051035 <ch051035@students.cs.ubc.ca>
2025-01-17 16:05:35 -0800

    image tilt

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,3 +51,27 @@ const header = document.querySelector("[data-header]");
 window.addEventListener("scroll", function () {
     header.classList[window.scrollY > 100 ? "add" : "remove"]("active")
 });
+
+
+/**
+ * Element tilt effect
+ */
+
+const tiltElements = document.querySelectorAll("[data-tilt]");
+
+const initTilt = function (event) {
+    /** get tilt element center position */
+    const centerX = this.offsetWidth / 2;
+    const centerY = this.offsetHeight / 2;
+
+    const tiltPosY = ((event.offsetX - centerX) / centerX) * 10;
+    const tiltPosX = ((event.offsetY - centerY) / centerY) * 10;
+
+    this.style.transform = `perspective(1000px) rotateX(${tiltPosX}deg) rotateY(${-tiltPosY}deg)`
+}
+
+addEventOnElements(tiltElements, "mousemove", initTilt);
+
+addEventOnElements(tiltElements, "mouseout", function () {
+    this.style.transform = `perspective(1000px) rotateX(0deg) rotateY(0deg)`
+})
